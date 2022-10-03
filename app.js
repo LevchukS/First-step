52e48615e619ff6d5b9ecf5e2e5909cbb076a31f
Serhii <95491075+LevchukS@users.noreply.github.com>
2022-10-03 15:48:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,64 +1,57 @@
-//task1
-
-const arr = ["Ace of hearts", "Ace of diamonds", "Ace of clubs", "Ace of spades",
-"King of hearts", "King of diamonds", "King of clubs", "King of spades",
-"Queen of hearts", "Queen of diamonds", "Queen of clubs", "Queen of spades",
-"Jack of hearts", "Jack of diamonds", "Jack of clubs", "Jack of spades",
-"10 of hearts", "10 of diamonds", "10 of clubs", "10 of spades",
-"9 of hearts", "9 of diamonds", "9 of clubs", "9 of spades",
-"8 of hearts", "8 of diamonds", "8 of clubs", "8 of spades",
-"7 of hearts", "7 of diamonds", "7 of clubs", "7 of spades",
-"6 of hearts", "6 of diamonds", "6 of clubs", "6 of spades",
-"5 of hearts", "5 of diamonds", "5 of clubs", "5 of spades",
-"4 of hearts", "4 of diamonds", "4 of clubs", "4 of spades",
-"3 of hearts", "3 of diamonds", "3 of clubs", "3 of spades",
-"2 of hearts", "2 of diamonds", "2 of clubs", "2 of spades",
-"Joker#1", "Joker#2"];
-const shuffle = (arr) => {
-    return arr.sort(() => Math.round(Math.random() * 100) - 46);
+const getCovidURL = (code = 'Ukraine', year = 2020) => {
+    return `https://covid-19.dataflowkit.com/${ year }/${ code }`;
 }
 
-shuffle(arr);
-alert(arr[0]);
+const isValidDate = date => {
+    const before = Date.parse('Wed, 22 Jan 2020 00:00:00 GMT');
+    const now = Date.now();
+    const then = new Date(date);
 
+    if(now < before && now < then)  return false;
 
+    return /^\d{4}-\d{2}-\d{2}$/.test(date);
+}
 
+const isValidCountry = country => {
+    if(country.length != 7) return false;
+    if(!/[a-z]{7}/i.test(country)) return false;
 
+    return true;
+}   
 
-
-//task3
-let text = prompt("Enter text use uppercase and lowercase leters and numbers", "");
-
-let elemText = text.split("");
-let newElemText = [];
-function changeElemText() {
-
-  /*  for (i = 0; i < elemText.length; i++) {
-        switch (elemText[i]) {
-            case "[A-Z]":
-                elemText[i] = "[a-z]";
-                break;
-            case "[a-z]":
-                elemText[i] = "[A-Z]";
-                break;
-            case "[0-9]":
-                elemText[i] = "_";
-                break;
-        };*/
-        for(let i of elemText){
-            if(!isNaN(parseInt(elemText(i)))) {
-                newLineArr.push('_');
-            }
-            else if (i === i.toLowerCase()){
-                newElemText.push(i.toUpperCase());                    
-            }
-            else if (i === i.toUpperCase()){
-                newElemText.push(i.toLowerCase());                    
-            } 
-            else{
-                newElemText.push(i);
-    }
-        }
-    return alert(newElemText.join(""));
+const DOM = {
+    inpDate : document.querySelector('.inp-date'),
+    inpFrom : document.querySelector('.inp-from'),
+    btnCount : document.querySelector('.btn-count'),
+    outAnswer : document.querySelector('.answer')
 };
-console.log(changeElemText());
+
+const showAnswer = (totalCases) => {
+    const cases = totalCases ? '+new cases' : '0';
+    DOM.outAnswer.innerText = `Total cases ${ cases }%`;
+}
+
+
+DOM.btnCount.onclick = () => {
+    const date = DOM.inpDate.value;
+    const from = DOM.inpFrom.value;
+    const to = DOM.inpTo.value;
+
+    const isValidD = isValidDate(date);
+    const isValidCountryFrom = isValidCountry(from);
+    console.log(date, from);
+    console.log(isValidD, isValidCountryFrom);
+    if(isValidD && isValidCountryFrom){
+        const year = date.split('-')[0];
+        const fFrom = fetch(getCovidURL(from, year)).then(r => r.json());
+        const fTo = fetch(getCovidURL(to, year)).then(r => r.json());
+
+        Promise.all([fFrom]).then(values => {
+            const dates = values.flat().map(el => el.date);
+            console.log(dates);
+            const covidCases = dates.includes(date);
+
+            showAnswer(covidCases);
+        });
+    }
+}
